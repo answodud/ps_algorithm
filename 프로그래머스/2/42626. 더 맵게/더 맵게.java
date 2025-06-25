64a1d08e5d3b1590954b42c82c60ebedc8fbdb76
Jaeyoung Mun <124485699+answodud@users.noreply.github.com>
2025-06-25 23:44:35 +0900

    [level 2] Title: 더 맵게, Time: 1679.55 ms, Memory: 129 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+        for(int scov : scoville){
+            pq.add(scov);
+        }
+        
+        
+        while(true){
+            if(pq.isEmpty()){
+                answer = -1;
+                break;
+            } else {
+                int one = pq.poll();
+                if(one >= K){
+                    break;
+                } else {
+                    if(pq.isEmpty()){
+                        answer = -1;
+                        break;
+                    } else {
+                        int two = pq.poll();
+                        int shake = one + two * 2;
+                        answer += 1;
+                        pq.add(shake);
+                    }
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
