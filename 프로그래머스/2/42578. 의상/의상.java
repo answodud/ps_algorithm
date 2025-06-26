2e1e64143dde9c6b2a3995297932adb2af1b98c8
Jaeyoung Mun <124485699+answodud@users.noreply.github.com>
2025-06-26 22:51:18 +0900

    [level 2] Title: 의상, Time: 0.11 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,5 +1,4 @@
 import java.util.*;
-import java.util.Map.*;
 
 class Solution {
     public int solution(String[][] clothes) {
@@ -9,14 +8,15 @@ class Solution {
         
         for(String[] cloth : clothes){
             if(map.getOrDefault(cloth[1], 0) == 0){
-                map.put(cloth[1], 1);
+               map.put(cloth[1], 1); 
             } else {
                 map.replace(cloth[1], map.get(cloth[1]) + 1);
             }
         }
+    
         
-        for(Entry<String, Integer> entry : map.entrySet()){
-            answer = answer * (entry.getValue() + 1);
+        for(String cloth : map.keySet()){
+            answer = answer * (map.get(cloth) + 1);
         }
         
         answer -= 1;
