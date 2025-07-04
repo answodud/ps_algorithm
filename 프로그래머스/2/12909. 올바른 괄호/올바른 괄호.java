555202d4289a747b4c55010ff062ad9a8823e328
Jaeyoung Mun <124485699+answodud@users.noreply.github.com>
2025-07-04 15:48:20 +0900

    [level 2] Title: 올바른 괄호, Time: 19.51 ms, Memory: 55.1 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,5 +1,4 @@
 import java.util.*;
-
 class Solution {
     boolean solution(String s) {
         boolean answer = true;
@@ -9,7 +8,7 @@ class Solution {
         for(int i = 0; i < s.length(); i++){
             if(s.charAt(i) == '('){
                 st.push(1);
-            } else{
+            } else {
                 if(st.empty()){
                     answer = false;
                     break;
