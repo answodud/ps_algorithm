6a5b67d4f9b5933ab54811881bbccd50f85a10f8
Jaeyoung Mun <124485699+answodud@users.noreply.github.com>
2025-07-04 11:45:17 +0900

    [level 2] Title: 올바른 괄호, Time: 21.31 ms, Memory: 54.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        
+        Stack<Integer> st = new Stack<>();
+        
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == '('){
+                st.push(1);
+            } else{
+                if(st.empty()){
+                    answer = false;
+                    break;
+                } else {
+                    st.pop();
+                }
+            }
+        }
+        
+        if(st.contains(1)){
+            answer = false;
+        }
+
+        return answer;
+    }
+}
