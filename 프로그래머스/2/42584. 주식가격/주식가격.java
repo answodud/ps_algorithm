6de02d9c9b8f3fd3ead5d4e65133a45b772f3089
Jaeyoung Mun <124485699+answodud@users.noreply.github.com>
2025-07-04 15:18:00 +0900

    [level 2] Title: 주식가격, Time: 20.59 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,16 +1,13 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
         
         for(int i = 0; i < prices.length - 1; i++){
-            int price = prices[i];
             int time = 0;
             for(int j = i + 1; j < prices.length; j++){
-                if(price <= prices[j]){
+                if(prices[i] <= prices[j]){
                     time++;
-                } else {
+                } else{
                     time++;
                     break;
                 }
