27f695e1959d5826df8e3e49ac5e66281ec4adf4
Jaeyoung Mun <124485699+answodud@users.noreply.github.com>
2025-07-04 13:30:10 +0900

    [level 1] Title: K번째수, Time: 1.23 ms, Memory: 88.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        
+        int i = 0;
+        for(int[] command : commands){
+            int[] arr = Arrays.copyOfRange(array, command[0] - 1, command[1]);
+            Arrays.sort(arr);
+            answer[i] = arr[command[2] - 1];
+            i++;
+        }
+        
+        return answer;
+    }
+}
