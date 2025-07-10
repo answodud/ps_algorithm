539a28e3397a4324deda9fbbbb068bc99de67f04
Jaeyoung Mun <124485699+answodud@users.noreply.github.com>
2025-07-11 07:41:53 +0900

    [level 2] Title: 모음 사전, Time: 9.43 ms, Memory: 85.9 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,30 +1,30 @@
 import java.util.*;
+
 class Solution {
     
-    ArrayList<String> list;
     String[] str = {"A", "E", "I", "O", "U"};
+    ArrayList<String> list = new ArrayList<>();
     
     public int solution(String word) {
-        list = new ArrayList<>();
+        dfs(word, "");
         int answer = 0;
-        dfs("");
         while(true){
             if(list.get(answer).equals(word)){
                 break;
             }
             answer++;
         }
-        
         return answer;
+        
     }
     
-    public void dfs(String cur){
-        list.add(cur);
-        if(cur.length() >= 5){
+    public void dfs(String word, String cur){
+        if(cur.length() > 5){
             return;
         }
+        list.add(cur);
         for(int i = 0; i < 5; i++){
-            dfs(cur + str[i]);
+            dfs(word, cur + str[i]);
         }
     }
 }
