727bbce72ff1433d06e37a94a93fe48aee2faea2
Jaeyoung Mun <124485699+answodud@users.noreply.github.com>
2025-07-11 09:50:37 +0900

    [level 1] Title: 모의고사, Time: 3.67 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -3,35 +3,36 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] answers) {
         int[] answer = {};
-        
-        int[] one = {1,2,3,4,5};
-        int[] two = {2,1,2,3,2,4,2,5};
-        int[] three = {3,3,1,1,2,2,4,4,5,5};
+        ArrayList<Integer> list = new ArrayList<>();
+        int[] a = {1,2,3,4,5};
+        int[] b = {2,1,2,3,2,4,2,5};
+        int[] c = {3,3,1,1,2,2,4,4,5,5};
         
         int[] score = new int[3];
         
         for(int i = 0; i < answers.length; i++){
-            if(one[i % 5] == answers[i]){
+            if(a[i % 5] == answers[i]){
                 score[0]++;
             }
-            if(two[i % 8] == answers[i]){
+              if(b[i % 8] == answers[i]){
                 score[1]++;
             }
-            if(three[i % 10] == answers[i]){
+              if(c[i % 10] == answers[i]){
                 score[2]++;
             }
         }
-        
+        System.out.println(score[0]);
+          System.out.println(score[1]);
+          System.out.println(score[2]);
         int max = Math.max(score[0], Math.max(score[1], score[2]));
         
-        ArrayList<Integer> list = new ArrayList<>();
         for(int i = 0; i < 3; i++){
-            if(score[i] == max){
-                list.add(i+1);
+            if(max == score[i]){
+                list.add(i + 1);
             }
         }
         
-        answer = list.stream().mapToInt(i->i).toArray();
+        answer = list.stream().mapToInt(i -> i).toArray();
         
         return answer;
     }
