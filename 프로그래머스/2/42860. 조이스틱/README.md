250fcde4b0d1217ca8178da40650d6fe078ec767
Jaeyoung Mun <124485699+answodud@users.noreply.github.com>
2025-07-25 20:50:50 +0900

    [level 2] Title: 조이스틱, Time: 0.07 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/README.md
+++ 프로그래머스/2/42860. 조이스틱/README.md
@@ -1,10 +1,10 @@
 # [level 2] 조이스틱 - 42860 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42860?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42860) 
 
 ### 성능 요약
 
-메모리: 73.2 MB, 시간: 0.08 ms
+메모리: 80.9 MB, 시간: 0.07 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 25일 15:49:27
+2025년 07월 25일 20:50:48
 
 ### 문제 설명
 
--- 프로그래머스/2/42860. 조이스틱/조이스틱.java
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -1,31 +1,26 @@
 class Solution {
     public int solution(String name) {
-         int answer = 0;
-    int n = name.length();
-    // 상하 이동
-    for(int i = 0; i < n; i++){
-        char c = name.charAt(i);
-        answer += Math.min(c - 'A', 'Z' - c + 1);
-    }
-    
-    // 좌우 이동
-    
-    int minX = n - 1;
-    
-    for(int i = 0; i < n; i++){
-        int next = i + 1;
-        while(next < n && name.charAt(next) == 'A'){
-            next++;
+        int answer = 0;
+        int n = name.length();
+        
+        for(int i = 0; i < name.length(); i++){
+            char c = name.charAt(i);
+            answer += Math.min(c - 'A', 'Z' - c + 1);
         }
         
-        int straight = n - 1;
-        int goback = i + i + n - next;
-        int backgo = n - next + n - next + i;
-        minX = Math.min(minX, Math.min(goback, backgo));
-    }
-    
-    answer += minX;
-    
-    return answer;
+        int minX = n - 1;
+        for(int i = 0; i < n; i++){
+            int next = i + 1;
+            while(next < n && name.charAt(next) == 'A'){
+                next++;
+            }
+            int goback = i * 2 + n - next;
+            int backgo = (n - next) * 2 + i;
+            minX = Math.min(minX, Math.min(goback, backgo));
         }
+        
+        answer += minX;
+        
+        return answer;
+    }
 }
