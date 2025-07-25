8d4ee680812ced586b6e88ea04c68836f0eab610
Jaeyoung Mun <124485699+answodud@users.noreply.github.com>
2025-07-25 21:37:24 +0900

    [level 3] Title: N으로 표현, Time: 29.15 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -5,19 +5,17 @@ class Solution {
         int answer = 0;
         
         ArrayList<Set<Integer>> dp = new ArrayList<>();
-        
         for(int i = 0; i <= 8; i++){
             dp.add(new HashSet<>());
         }
         
         for(int i = 1; i <= 8; i++){
             
-            // N, NN, NNN 등을 저장
-            int cnt = 0;
+            int cur = 0;
             for(int k = 0; k < i; k++){
-                cnt = cnt * 10 + N;
+                cur = cur * 10 + N;
             }
-            dp.get(i).add(cnt);
+            dp.get(i).add(cur);
             
             for(int j = 1; j < i; j++){
                 for(int a : dp.get(j)){
