8de78427533e3896f535d583dc8ffe2888f2e41c
Jaeyoung Mun <124485699+answodud@users.noreply.github.com>
2025-08-01 16:28:08 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.49 ms, Memory: 55 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,11 +1,20 @@
 import java.util.*;
 
 class Solution {
-    
     boolean[][] visited;
     int N, M;
     int[] dx = {-1, 1, 0, 0};
     int[] dy = {0, 0, -1, 1};
+    
+    class Map {
+        int x;
+        int y;
+        
+        Map(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
+    }
     
     public int solution(int[][] maps) {
         int answer = 0;
@@ -13,7 +22,7 @@ class Solution {
         M = maps[0].length;
         
         visited = new boolean[N][M];
-        answer = bfs(0, 0, maps);
+        answer = bfs(0,0,maps);
         if(!visited[N-1][M-1]){
             return -1;
         }
@@ -22,28 +31,30 @@ class Solution {
     }
     
     public int bfs(int x, int y, int[][] maps){
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{x, y});
+        Queue<Map> q = new LinkedList<>();
+        q.offer(new Map(x, y));
         visited[x][y] = true;
         
         while(!q.isEmpty()){
-            int[] cur = q.poll();
-            int cx = cur[0];
-            int cy = cur[1];
+            Map cur = q.poll();
+            int cx = cur.x;
+            int cy = cur.y;
             
             for(int i = 0; i < 4; i++){
                 int nx = cx + dx[i];
                 int ny = cy + dy[i];
+                
                 if(nx >= 0 && ny >= 0 && nx < N && ny < M){
-                    if(!visited[nx][ny] && maps[nx][ny] != 0){
+                    if(maps[nx][ny] != 0 && !visited[nx][ny]){
                         visited[nx][ny] = true;
                         maps[nx][ny] = maps[cx][cy] + 1;
-                        q.offer(new int[]{nx, ny});
+                        q.offer(new Map(nx, ny));
+                    }
+                        
                     }
                 }
             }
-        }
-        
         return maps[N-1][M-1];
+        }
+    
     }
-}
