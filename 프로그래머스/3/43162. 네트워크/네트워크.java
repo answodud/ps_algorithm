7e6b2b7747187e0a4e371b4322f60d6d2e991962
Jaeyoung Mun <124485699+answodud@users.noreply.github.com>
2025-08-01 22:46:25 +0900

    [level 3] Title: 네트워크, Time: 0.82 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -7,44 +7,34 @@ class Solution {
         int answer = 0;
         graph = new ArrayList[n];
         visited = new boolean[n];
-        
         for(int i = 0; i < n; i++){
             graph[i] = new ArrayList<>();
         }
-        
         for(int i = 0; i < n; i++){
             for(int j = 0; j < n; j++){
-                if(i == j){
-                    continue;
-                }
                 if(computers[i][j] == 1){
                     graph[i].add(j);
-                    System.out.println(i + "와 " + j);
                 }
             }
         }
         
         for(int i = 0; i < n; i++){
             if(!visited[i]){
-                System.out.println(i + "에서 출발");
-                answer++;
                 dfs(i);
+                answer++;
             }
         }
-        
         
         return answer;
     }
     
-    public void dfs(int v){
-        System.out.println(v + " 방문");
-        visited[v] = true;
-        for(int next : graph[v]){
-            if(!visited[next]){
-                System.out.println(next + " 방문");
-                visited[next] = true;
-                dfs(next);
+    public void dfs(int start){
+        visited[start] = true;
+            for(int next : graph[start]){
+                if(!visited[next]){
+                    visited[next] = true;
+                    dfs(next);
+                }
             }
-        }
     }
 }
