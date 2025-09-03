de8d84e6fcadf0f2ffeaec1e3432d9b944a48e3e
Jaeyoung Mun <124485699+answodud@users.noreply.github.com>
2025-09-03 20:08:32 +0900

    [level 3] Title: 단어 변환, Time: 13.81 ms, Memory: 84.8 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,8 +1,7 @@
 import java.util.*;
 
 class Solution {
-    
-    class Word{
+    class Word {
         int step;
         String word;
         
@@ -13,42 +12,58 @@ class Solution {
     }
     
     boolean[] visited;
+    
     public int solution(String begin, String target, String[] words) {
         int answer = 0;
         visited = new boolean[words.length];
-        Queue<Word> q = new LinkedList<>();
-
-        q.offer(new Word(0, begin));
+        boolean x = false;
         
+        for(String word : words){
+            if(word.equals(target)){
+                x = true;
+                break;
+            }
+        }
+        
+        if(!x){
+            return 0;
+        }
+        
+        return bfs(begin, target, words);
+    }
+    
+    public int bfs(String begin, String target, String[] words){
+        Queue<Word> q = new LinkedList<>();
+        q.offer(new Word(0, begin));
         while(!q.isEmpty()){
             Word cur = q.poll();
+            System.out.println(cur.step + "번째 단어 " + cur.word);
             if(cur.word.equals(target)){
                 return cur.step;
             }
-            
             for(int i = 0; i < words.length; i++){
-                if(oneDiff(words[i], cur.word) && !visited[i]){
-                    visited[i] = true;
+                if(oneDiff(cur.word, words[i]) && !visited[i]){
                     q.offer(new Word(cur.step + 1, words[i]));
+                    visited[i] = true;
                 }
             }
         }
         
-        
-        return answer;
+        return 0;
     }
     
-    
-    public boolean oneDiff(String target, String word){
+    public boolean oneDiff(String cur, String next){
         int cnt = 0;
-        for(int i = 0; i < target.length(); i++){
-            if(target.charAt(i) != word.charAt(i)){
+        for(int i = 0; i < cur.length(); i++){
+            if(cur.charAt(i) != next.charAt(i)){
                 cnt++;
             }
-            if(cnt > 1){
-                return false;
-            }
         }
-        return true;
+        if(cnt == 1){
+            return true;
+        } else {
+            return false;
+        }
     }
+    
 }
