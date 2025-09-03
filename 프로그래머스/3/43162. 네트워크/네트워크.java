e25ab80331128b063815840d041d53d0d5af2032
Jaeyoung Mun <124485699+answodud@users.noreply.github.com>
2025-09-03 17:40:43 +0900

    [level 3] Title: 네트워크, Time: 8.98 ms, Memory: 81.1 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,26 +1,33 @@
 import java.util.*;
 
 class Solution {
+    
     ArrayList<Integer>[] graph;
+    int answer = 0;
     boolean[] visited;
+    
     public int solution(int n, int[][] computers) {
-        int answer = 0;
-        graph = new ArrayList[n];
-        visited = new boolean[n];
+        
+        graph = new ArrayList[n+1];
+        visited = new boolean[n+1];
+        
         for(int i = 0; i < n; i++){
             graph[i] = new ArrayList<>();
         }
         
+        // 간선 연결
         for(int i = 0; i < n; i++){
             for(int j = 0; j < n; j++){
                 if(computers[i][j] == 1){
                     graph[i].add(j);
+                    graph[j].add(i);
                 }
             }
         }
         
         for(int i = 0; i < n; i++){
             if(!visited[i]){
+                System.out.println(i + " 시작");
                 dfs(i);
                 answer++;
             }
@@ -29,11 +36,11 @@ class Solution {
         return answer;
     }
     
-    public void dfs(int start){
-        visited[start] = true;
-        for(int next : graph[start]){
-            if(!visited[next]){
-                visited[next] = true;
+    public void dfs(int v){
+        if(!visited[v]){
+            System.out.println(v + " 방문");
+            visited[v] = true;
+            for(int next : graph[v]){
                 dfs(next);
             }
         }
