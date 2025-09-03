32af030e1fdf403d62787437f2a888de8e39c897
Jaeyoung Mun <124485699+answodud@users.noreply.github.com>
2025-09-03 19:37:38 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.90 ms, Memory: 54.8 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,6 +1,7 @@
 import java.util.*;
 
 class Solution {
+    
     class Map {
         int x;
         int y;
@@ -9,46 +10,42 @@ class Solution {
             this.y = y;
         }
     }
-    boolean[][] visited;
-    int N, M;
-    int[] dx = {-1, 1, 0, 0};
-    int[] dy = {0, 0, -1, 1};
     
+    int[] ax = {-1, 1, 0, 0};
+    int[] ay = {0, 0, -1, 1};
+    int N, M;
+    boolean[][] visited;
     public int solution(int[][] maps) {
         int answer = 0;
         N = maps.length;
         M = maps[0].length;
-        visited = new boolean[N][M];
-        
-        answer = bfs(0, 0, maps);
+        visited = new boolean[N+1][M+1];
+        bfs(0, 0, maps);
         if(!visited[N-1][M-1]){
             return -1;
         }
-        
-        return answer;
+        return maps[N-1][M-1];
     }
     
-    public int bfs(int x, int y, int[][] maps){
+    public void bfs(int x, int y, int[][] maps){
         Queue<Map> q = new LinkedList<>();
         q.offer(new Map(x, y));
         visited[x][y] = true;
-        
         while(!q.isEmpty()){
             Map cur = q.poll();
-            for(int i = 0; i < 4; i++){
-                int nx = cur.x + dx[i];
-                int ny = cur.y + dy[i];
-                if(nx >= 0 && ny >= 0 && nx < N && ny < M){
-                    if(!visited[nx][ny] && maps[nx][ny] != 0){
-                        q.offer(new Map(nx, ny));
-                        visited[nx][ny] = true;
-                        maps[nx][ny] = maps[cur.x][cur.y] + 1;
+        for(int i = 0; i < 4; i++){
+                    int nx = cur.x + ax[i];
+                    int ny = cur.y + ay[i];
+                    if(nx >= 0 && ny >= 0 && nx < N && ny < M){
+                        if(maps[nx][ny] != 0 && !visited[nx][ny]){
+                            maps[nx][ny] = maps[cur.x][cur.y] + 1;
+                            q.offer(new Map(nx, ny));
+                            visited[nx][ny] = true;
+                        }
                     }
-                }
             }
         }
         
-        return maps[N-1][M-1];
-        
     }
+    
 }
