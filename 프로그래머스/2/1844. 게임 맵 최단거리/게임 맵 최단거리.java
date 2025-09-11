0da39620c46e84d2de43d339ffe1e4c3631014a0
Jaeyoung Mun <124485699+answodud@users.noreply.github.com>
2025-09-11 14:51:04 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 13.30 ms, Memory: 55.7 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -2,6 +2,12 @@ import java.util.*;
 
 class Solution {
     
+    int[] dx = {-1, 1, 0, 0};
+    int[] dy = {0, 0, -1, 1};
+    int[][] maps;
+    int N, M;
+    boolean visited[][];
+    
     class Map {
         int x;
         int y;
@@ -11,38 +17,41 @@ class Solution {
         }
     }
     
-    int[] ax = {-1, 1, 0, 0};
-    int[] ay = {0, 0, -1, 1};
-    int N, M;
-    boolean[][] visited;
     public int solution(int[][] maps) {
         int answer = 0;
+        this.maps = maps;
         N = maps.length;
         M = maps[0].length;
-        visited = new boolean[N+1][M+1];
-        bfs(0, 0, maps);
+        visited = new boolean[N][M];
+        
+        bfs(0, 0);
+        
         if(!visited[N-1][M-1]){
             return -1;
         }
+        
         return maps[N-1][M-1];
     }
     
-    public void bfs(int x, int y, int[][] maps){
+    public void bfs(int x, int y){
         Queue<Map> q = new LinkedList<>();
         q.offer(new Map(x, y));
         visited[x][y] = true;
+        
         while(!q.isEmpty()){
             Map cur = q.poll();
-        for(int i = 0; i < 4; i++){
-                    int nx = cur.x + ax[i];
-                    int ny = cur.y + ay[i];
+            for(int i = 0; i < 4; i++){
+                for(int j = 0; j < 4; j++){
+                    int nx = cur.x + dx[i];
+                    int ny = cur.y + dy[i];
                     if(nx >= 0 && ny >= 0 && nx < N && ny < M){
-                        if(maps[nx][ny] != 0 && !visited[nx][ny]){
+                        if(!visited[nx][ny] && maps[nx][ny] != 0){
                             maps[nx][ny] = maps[cur.x][cur.y] + 1;
                             q.offer(new Map(nx, ny));
                             visited[nx][ny] = true;
                         }
                     }
+                }
             }
         }
         
