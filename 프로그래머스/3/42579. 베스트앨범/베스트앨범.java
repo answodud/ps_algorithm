6595cec2179257b6f0d66ef882031f35ddf05551
Jaeyoung Mun <124485699+answodud@users.noreply.github.com>
2025-09-18 20:45:52 +0900

    [level 3] Title: 베스트앨범, Time: 16.50 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -2,44 +2,40 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
-        int[] answer = {};
         
-        HashMap<String, Integer> genreSong = new HashMap<>();
+        // 장르별 노래 횟수 저장 용도
+        HashMap<String, Integer> map = new HashMap<>();
         for(int i = 0; i < genres.length; i++){
-            if(genreSong.getOrDefault(genres[i], 0) != 0){
-                genreSong.replace(genres[i], genreSong.get(genres[i]) + plays[i]);
+            if(map.containsKey(genres[i])){
+                map.replace(genres[i], map.get(genres[i]) + plays[i]);
             } else {
-                genreSong.put(genres[i], plays[i]);
+                map.put(genres[i], plays[i]);
             }
         }
         
-        List<String> mostGenres = new ArrayList(genreSong.keySet());
-        Collections.sort(mostGenres, (v1, v2) -> genreSong.get(v2) - genreSong.get(v1));
+        // 인기 장르 순으로 정렬(내림차순)
+        List<String> topGenres = new ArrayList(map.keySet());
+        Collections.sort(topGenres, (v1, v2) -> map.get(v2) - map.get(v1));
         
-        HashMap<String, HashMap<Integer, Integer>> map = new HashMap<>();
+        List<Integer> answer = new ArrayList<>();
+        HashMap<String, HashMap<Integer, Integer>> music = new HashMap<>();
+        
         for(int i = 0; i < genres.length; i++){
-                if(map.containsKey(genres[i])){
-                    map.get(genres[i]).put(i, plays[i]);
-                } else {
-                    HashMap<Integer, Integer> sMap = new HashMap<>();
-                    sMap.put(i, plays[i]);
-                    map.put(genres[i], sMap);
-                }
-            }
-        
-        List<Integer> answerList = new ArrayList<>();
-        
-        for(String g : mostGenres){
-            HashMap<Integer, Integer> sMap = map.get(g);
-            List<Integer> music = new ArrayList(sMap.keySet());
-            Collections.sort(music, (v1, v2) -> sMap.get(v2) - sMap.get(v1));
-            answerList.add(music.get(0)); // 제일 많이 재생된 곡 넣기
-                if(music.size() > 1){
-                    answerList.add(music.get(1));
-                }
+            music.computeIfAbsent(genres[i], k -> new HashMap<>()).put(i, plays[i]);
         }
         
-        return answerList.stream().mapToInt(i -> i).toArray();
+        for(String gen : topGenres){
+            if(music.containsKey(gen)){
+                HashMap<Integer, Integer> sMap = music.get(gen);
+                List<Integer> cnt = new ArrayList(sMap.keySet());
+                Collections.sort(cnt, (v1, v2) -> sMap.get(v2) - sMap.get(v1));
+                answer.add(cnt.get(0));
+                if(sMap.size() > 1){
+                    answer.add(cnt.get(1));
+                }
+            }
+        }
         
+        return answer.stream().mapToInt(i->i).toArray();
     }
 }
