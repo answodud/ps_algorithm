13d804e0f3f388419e093390b84e24c346f5a704
Jaeyoung Mun <124485699+answodud@users.noreply.github.com>
2025-09-18 14:15:20 +0900

    [level 2] Title: H-Index, Time: 0.91 ms, Memory: 89 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,21 +1,22 @@
 import java.util.*;
+
 class Solution {
     public int solution(int[] citations) {
         int answer = 0;
-        
         Arrays.sort(citations);
         
         for(int i = 0; i < citations.length; i++){
-            if(citations[i] == (citations.length - i)){
+            if(citations.length - i == citations[i]){
                 answer = citations[i];
                 break;
-            } else if(citations[i] < (citations.length - i)){
+            } else if(citations[i] < citations.length - i){
                 continue;
             } else {
                 answer = citations.length - i;
-                 break;
+                break;
             }
         }
+        
         
         return answer;
     }
