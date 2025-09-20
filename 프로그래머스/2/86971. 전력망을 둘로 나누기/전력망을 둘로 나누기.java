2921b5be193cd9d861734b8d67617bbbca7445af
Jaeyoung Mun <124485699+answodud@users.noreply.github.com>
2025-09-20 22:04:46 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 2.26 ms, Memory: 93.3 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -3,12 +3,13 @@ import java.util.*;
 class Solution {
     
     ArrayList<Integer>[] graph;
+    boolean[] visited;
     public int solution(int n, int[][] wires) {
-        
+        int answer = 100001;
         graph = new ArrayList[n+1];
-        int min = 100001;
         
-        for(int i = 1; i <= n; i++){
+        
+        for(int i = 0; i <= n; i++){
             graph[i] = new ArrayList<>();
         }
         
@@ -19,36 +20,35 @@ class Solution {
             graph[v2].add(v1);
         }
         
+        // 하나씩 끊고
         for(int i = 0; i < wires.length; i++){
             int v1 = wires[i][0];
             int v2 = wires[i][1];
             graph[v1].remove(Integer.valueOf(v2));
             graph[v2].remove(Integer.valueOf(v1));
             
-            boolean[] visited = new boolean[n+1];
-            int cnt = dfs(1, visited);
-            int diff = Math.abs(cnt - (n - cnt));
-            min = Math.min(min, diff);
+            // 방문 배열 항상 초기화
+            visited = new boolean[n+1];
             
+            int n1 = dfs(1);
+            answer = Math.min(Math.abs(n1 - (n - n1)), answer);
+            
+            // 원상복구
             graph[v1].add(v2);
             graph[v2].add(v1);
         }
         
-        
-        return min;
+        return answer;
     }
     
-    public int dfs(int v, boolean[] visited){
+    public int dfs(int v){
         visited[v] = true;
-        int cnt = 1;
-        
+        int cnt = 1; // 방문한 본인 포함
         for(int next : graph[v]){
             if(!visited[next]){
-                visited[next] = true;
-                cnt += dfs(next, visited);
+                cnt += dfs(next);
             }
         }
-        
         return cnt;
     }
 }
