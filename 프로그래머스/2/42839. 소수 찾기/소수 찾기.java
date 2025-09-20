cb04d7cb66a10f309671b2337986cd5a17e264ba
Jaeyoung Mun <124485699+answodud@users.noreply.github.com>
2025-09-20 20:37:25 +0900

    [level 2] Title: 소수 찾기, Time: 24.48 ms, Memory: 84.9 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,43 +1,48 @@
 import java.util.*;
+
 class Solution {
     
+    String numbers;
     boolean[] visited;
-    HashSet<Integer> hashSet = new HashSet<>();
+    int answer = 0;
+    ArrayList<Integer> list = new ArrayList<>();
     
     public int solution(String numbers) {
-        int answer = 0;
-        visited = new boolean[numbers.length()];
-        dfs(numbers, "", 0);
         
-        for(int n : hashSet){
-            if(isPrime(n)){
-                answer++;
-            }
-        }
+        this.numbers = numbers;
+        visited = new boolean[numbers.length()];
+        
+        dfs("", 0);
         
         return answer;
     }
     
-    public void dfs(String numbers, String cur, int depth){
-        if(depth > numbers.length()){
-            return;
+    public void dfs(String cur, int depth){
+        if(cur.length() > 0){
+            int x = Integer.valueOf(cur);
+            if(isPrime(x)){
+                if(!list.contains(x)){
+                    answer++;
+                    list.add(x);
+                }
+            }
         }
+        
         for(int i = 0; i < numbers.length(); i++){
             if(!visited[i]){
                 visited[i] = true;
-                hashSet.add(Integer.valueOf(cur + numbers.charAt(i)));
-                dfs(numbers, cur + numbers.charAt(i), depth + 1);
+                dfs(cur + numbers.charAt(i), depth + 1);
                 visited[i] = false;
             }
-        }
+        }   
     }
     
-    public boolean isPrime(int n){
-        if(n <= 1){
+    public boolean isPrime(int num){
+        if(num < 2){
             return false;
         }
-        for(int i = 2; i <= Math.sqrt(n); i++){
-            if(n % i == 0){
+        for(int i = 2; i <= Math.sqrt(num); i++){
+            if(num % i == 0){
                 return false;
             }
         }
