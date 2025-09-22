09a1722613f76ede52483a79355fe0c819c1adcb
Jaeyoung Mun <124485699+answodud@users.noreply.github.com>
2025-09-23 02:30:41 +0900

    [level 2] Title: 조이스틱, Time: 0.06 ms, Memory: 69 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.java
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -1,26 +1,30 @@
 class Solution {
     public int solution(String name) {
         int answer = 0;
+        int cnt = 0;
+        
+        // 상하이동
+        for(int i = 0; i < name.length(); i++){
+            char x = name.charAt(i);
+            cnt += Math.min(x - 'A', 'Z' - x + 1);
+        }
         int n = name.length();
         
+        int move = n - 1;
+        // 좌우이동
         for(int i = 0; i < name.length(); i++){
-            char c = name.charAt(i);
-            answer += Math.min(c - 'A', 'Z' - c + 1);
-        }
-        
-        int minX = n - 1;
-        for(int i = 0; i < n; i++){
             int next = i + 1;
+            // 다음 이동 위치 파악하기
             while(next < n && name.charAt(next) == 'A'){
                 next++;
             }
-            int goback = i * 2 + n - next;
-            int backgo = (n - next) * 2 + i;
-            minX = Math.min(minX, Math.min(goback, backgo));
+            int goback = i + i + (n - next);
+            int backgo = (n - next) + (n - next) + i;
+            move = Math.min(move, Math.min(goback, backgo));
         }
         
-        answer += minX;
+        cnt += move;
         
-        return answer;
+        return cnt;
     }
 }
