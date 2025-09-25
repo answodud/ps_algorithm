218c45e36cab97ea52d62be3533f7ff99fe1777f
Jaeyoung Mun <124485699+answodud@users.noreply.github.com>
2025-09-25 10:15:19 +0900

    [level 3] Title: 순위, Time: 11.34 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.java
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -1,26 +1,33 @@
 class Solution {
     public int solution(int n, int[][] results) {
         int answer = 0;
-        boolean[][] graph = new boolean[n+1][n+1];
+        
+        // a가 b를 이기고, b가 c를 이기면, a가 c도 이김
+        
+        int[][] arr = new int[n+1][n+1];
         
         for(int i = 0; i < results.length; i++){
-            graph[results[i][0]][results[i][1]] = true;
+            int win = results[i][0];
+            int lose = results[i][1];
+            arr[win][lose] = 1;
+            arr[lose][win] = -1;
         }
         
         for(int k = 1; k <= n; k++){
             for(int i = 1; i <= n; i++){
                 for(int j = 1; j <= n; j++){
-                    if(graph[i][k] && graph[k][j]){
-                        graph[i][j] = true;
+                    if(arr[i][k] == 1 && arr[k][j] == 1){
+                        arr[i][j] = 1;
+                        arr[j][i] = -1;
                     }
                 }
             }
         }
         
-        for(int i = 1; i <= n; i++){
+        for(int i = 1; i < arr.length; i++){
             int cnt = 0;
-            for(int j = 1; j <= n; j++){
-                if(graph[i][j] || graph[j][i]){
+            for(int j = 1; j < arr[i].length; j++){
+                if(arr[i][j] != 0){
                     cnt++;
                 }
             }
