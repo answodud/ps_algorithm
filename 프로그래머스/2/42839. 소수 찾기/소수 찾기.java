ed48644ea2a1a64c351f7ad86d2e9e13b875efe2
Jaeyoung Mun <124485699+answodud@users.noreply.github.com>
2025-09-25 13:19:38 +0900

    [level 2] Title: 소수 찾기, Time: 23.93 ms, Memory: 82.9 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,40 +1,42 @@
 import java.util.*;
 
 class Solution {
-    
     String numbers;
     boolean[] visited;
-    int answer = 0;
-    ArrayList<Integer> list = new ArrayList<>();
+    HashSet<Integer> hashSet = new HashSet<>();
     
     public int solution(String numbers) {
-        
+        int answer = 0;
         this.numbers = numbers;
         visited = new boolean[numbers.length()];
         
-        dfs("", 0);
         
-        return answer;
-    }
-    
-    public void dfs(String cur, int depth){
-        if(cur.length() > 0){
-            int x = Integer.valueOf(cur);
-            if(isPrime(x)){
-                if(!list.contains(x)){
-                    answer++;
-                    list.add(x);
-                }
-            }
+        int[] num = new int[numbers.length()];
+        
+        for(int i = 0; i < numbers.length(); i++){
+            num[i] = Integer.valueOf(numbers.charAt(i));
         }
         
+        dfs("");
+        
+        
+        return hashSet.size();
+    }
+    
+    public void dfs(String cur){
+        if(cur.length() > 0){
+            int x = Integer.parseInt(cur);
+        if(isPrime(x)){
+            hashSet.add(x);
+        }
+        }
         for(int i = 0; i < numbers.length(); i++){
             if(!visited[i]){
                 visited[i] = true;
-                dfs(cur + numbers.charAt(i), depth + 1);
+                dfs(cur + numbers.charAt(i));
                 visited[i] = false;
             }
-        }   
+        }
     }
     
     public boolean isPrime(int num){
