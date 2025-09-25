e5356897f40ec9d00dec0aa246974020dbda20c6
Jaeyoung Mun <124485699+answodud@users.noreply.github.com>
2025-09-25 11:01:36 +0900

    [level 3] Title: N으로 표현, Time: 25.05 ms, Memory: 85.4 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -2,29 +2,29 @@ import java.util.*;
 
 class Solution {
     public int solution(int N, int number) {
-        int answer = 0;
-        ArrayList<Set<Integer>> dp = new ArrayList<>();
+        int answer = -1;
         
+        ArrayList<HashSet<Integer>> dp = new ArrayList<>();
         for(int i = 0; i <= 8; i++){
             dp.add(new HashSet<>());
         }
         
         for(int i = 1; i <= 8; i++){
             int cur = 0;
-            for(int k = 0; k < i; k++){
+            for(int k = 1; k <= i; k++){
                 cur = cur * 10 + N;
             }
             dp.get(i).add(cur);
             
             for(int j = 0; j < i; j++){
-                for(int a : dp.get(j)){
-                    for(int b : dp.get(i - j)){
-                        dp.get(i).add(a + b);
-                         dp.get(i).add(a - b);
-                         dp.get(i).add(a * b);
-                         if(b != 0){
-                             dp.get(i).add(a / b);
-                         }
+                for(int x : dp.get(j)){
+                    for(int y : dp.get(i - j)){
+                        dp.get(i).add(x + y);
+                        dp.get(i).add(x - y);
+                        dp.get(i).add(x * y);
+                        if(y != 0){
+                            dp.get(i).add(x / y);
+                        }
                     }
                 }
             }
@@ -35,6 +35,8 @@ class Solution {
             
         }
         
-        return -1;
+        
+        
+        return answer;
     }
 }
