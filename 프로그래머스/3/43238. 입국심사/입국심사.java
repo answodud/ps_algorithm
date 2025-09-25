80252aa7d3df110bb895b173b1c701b23a6dcabe
Jaeyoung Mun <124485699+answodud@users.noreply.github.com>
2025-09-25 09:17:04 +0900

    [level 3] Title: 입국심사, Time: 87.16 ms, Memory: 85.9 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -3,30 +3,25 @@ import java.util.*;
 class Solution {
     public long solution(int n, int[] times) {
         long answer = 0;
-        long left = 1;
-        int maxTime = 0;
-        for(int time : times){
-            maxTime = Math.max(maxTime, time);
-        }
-        long right = (long) maxTime * n;
+        
+        Arrays.sort(times);
+        long left = 0;
+        long right = times[times.length - 1] * (long)n;
         
         while(left <= right){
-            
             long mid = (left + right) / 2;
-            long count = 0;
-            
+            long cnt = 0;
             for(int time : times){
-                count += mid / time;
+                cnt += mid / time;
             }
-            
-            if(count >= n){
-                answer = mid;
+            if(cnt >= n){
                 right = mid - 1;
+                answer = mid;
             } else {
                 left = mid + 1;
             }
-            
         }
+        
         
         return answer;
     }
