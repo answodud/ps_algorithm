2eb7d50a65e87cb29f78730d2936f5a84a292800
Jaeyoung Mun <124485699+answodud@users.noreply.github.com>
2025-10-02 14:52:02 +0900

    [level 3] Title: 입국심사, Time: 102.68 ms, Memory: 87.6 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -8,18 +8,21 @@ class Solution {
         
         long left = 0; // 최소 시간
         long right = times[times.length - 1] * (long)n; // 최대 시간
-        
+       
         while(left <= right){
-            long mid = (left + right) / 2;
             long cnt = 0;
+            long mid = (left + right) / 2;
             for(int time : times){
                 cnt += mid / time;
             }
-            
+            // 심사받을 수 있는 사람 수가 이미 n을 넘었거나 같으면
             if(cnt >= n){
+                // left ~ mid - 1 부분 탐색 + answer에 일단 mid 넣기
                 right = mid - 1;
                 answer = mid;
             } else {
+                // 심사받을 수 있는 수가 적으면
+                // mid + 1 ~ right 탐색
                 left = mid + 1;
             }
         }
