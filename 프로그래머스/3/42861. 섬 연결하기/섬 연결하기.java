7121a164c2e1cd1542df289229b385bd97be56d8
Jaeyoung Mun <124485699+answodud@users.noreply.github.com>
2025-10-02 14:45:39 +0900

    [level 3] Title: 섬 연결하기, Time: 0.80 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -2,44 +2,46 @@ import java.util.*;
 
 class Solution {
     
+    boolean[] visited;
     int[] parent;
-    
     public int solution(int n, int[][] costs) {
         int answer = 0;
+        visited = new boolean[n];
         parent = new int[n];
         
         for(int i = 0; i < n; i++){
             parent[i] = i;
         }
         
-        // 간선 가중치 기준 오름차순으로 정렬
+        // 배열을 비용 기준 오름차순으로 정렬
         Arrays.sort(costs, (o1, o2) -> o1[2] - o2[2]);
         
         for(int i = 0; i < costs.length; i++){
-            int a = costs[i][0];
-            int b = costs[i][1];
-            if(find(a) != find(b)){
-                union(a, b);
-                answer+=costs[i][2];
+            if(find(costs[i][0]) != find(costs[i][1])){
+                answer += costs[i][2];
+                union(costs[i][0], costs[i][1]);
             }
         }
-        
         
         return answer;
     }
     
-    public void union(int a, int b){
-        a = find(a);
-        b = find(b);
-        if(a != b){
-            parent[b] = a;
+    public int find(int x){
+        // x의 부모가 x면 x를 그냥 return
+        if(parent[x] == x){
+            return x;
+        } else {
+            // 아니라면 x를 
+            return x = find(parent[x]);
         }
     }
     
-    public int find(int a){
-        if(parent[a] == a){
-            return a;
+    // 합치기
+    public void union(int x, int y){
+        x = find(x); // x의 가장 부모 찾기
+        y = find(y); // y의 가장 부모 찾기
+        if(x != y){ // 같은 집합이 아니라면 합쳐주기
+            parent[y] = x;
         }
-        return parent[a] = find(parent[a]);
     }
 }
