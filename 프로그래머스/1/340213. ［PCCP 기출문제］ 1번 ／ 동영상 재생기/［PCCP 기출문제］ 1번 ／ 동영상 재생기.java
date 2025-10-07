514560de4648838883a93777a22c2d625fb8a3ad
Jaeyoung Mun <124485699+answodud@users.noreply.github.com>
2025-10-08 03:08:56 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 1.12 ms, Memory: 90.3 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
@@ -1,49 +1,48 @@
-import java.util.*;
-
 class Solution {
     public String solution(String video_len, String pos, String op_start, String op_end, String[] commands) {
         String answer = "";
-        int V = toSec(video_len);
-        int cur = toSec(pos);
-        int ops = toSec(op_start);
-        int ope = toSec(op_end);
         
-        for(String command : commands){
-            if(cur >= ops && cur <= ope){
-                cur = ope;
-            }
-            if(command.equals("prev")){
-                if(cur < 10){
-                    cur = 0;
-                } else {
-                    cur -= 10;
-                }
-                if(cur >= ops && cur <= ope){
-                    cur = ope;
-                }
-            } else {
-                if(cur + 10 > V){
-                    cur = V;
-                } else {
-                    cur += 10;
-                }
-                if(cur >= ops && cur <= ope){
-                    cur = ope;
-                }
-            }
+        int cur = toSec(pos);
+        int start = toSec(op_start);
+        int end = toSec(op_end);
+        int len = toSec(video_len);
+        
+        if(cur >= start && cur <= end){
+            cur = end;
         }
         
-        return mmss(cur);
+        for(String command : commands){
+            if(command.equals("prev")){
+                if(cur - 10 < 0){
+                    cur = 0;
+                } else {
+                    cur = cur - 10;
+                }
+            } else if(command.equals("next")){
+                if(cur + 10 > len){
+                    cur = len;
+                } else {
+                    cur = cur + 10;
+                }
+            }
+             if(cur >= start && cur <= end){
+                    cur = end;}
+        }
+        
+        return toStr(cur);
     }
     
-    public int toSec(String s){
-        String mmss[] = s.split(":");
-        return Integer.valueOf(mmss[0]) * 60 + Integer.valueOf(mmss[1]);
+    public int toSec(String time){
+        String[] str = time.split(":");
+        int sec = 0;
+        sec += Integer.valueOf(str[0]) * 60;
+        sec += Integer.valueOf(str[1]);
+        return sec;
     }
     
-    public String mmss(int x){
-        int mm = x / 60;
-        int ss = x % 60;
-        return String.format("%02d:%02d", mm, ss);
+    public String toStr(int time){
+        int min = time / 60;
+        int sec = time % 60;
+        return String.format("%02d:%02d", min, sec);
     }
 }
