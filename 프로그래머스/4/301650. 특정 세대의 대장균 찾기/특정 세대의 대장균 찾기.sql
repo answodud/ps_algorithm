a3093613f571b86eb1ff90aff2049c2182781781
Jaeyoung Mun <124485699+answodud@users.noreply.github.com>
2025-10-26 01:33:35 +0900

    [level 4] Title: 특정 세대의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
+++ 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
@@ -3,4 +3,5 @@ select id
 from ecoli_data
 where parent_id in
 (select id from ecoli_data where parent_id in
-(select id from ecoli_data where parent_id is null));
+(select id from ecoli_data where parent_id is null))
+order by id;
