37cda08802e8723702309cb2ac09ad7b9c061273
Jaeyoung Mun <124485699+answodud@users.noreply.github.com>
2025-10-28 23:12:56 +0900

    [level 3] Title: 특정 조건을 만족하는 물고기별 수와 최대 길이 구하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/3/298519. 특정 조건을 만족하는 물고기별 수와 최대 길이 구하기/특정 조건을 만족하는 물고기별 수와 최대 길이 구하기.sql
@@ -0,0 +1,14 @@
+-- FISH_INFO에서 평균 길이가 33cm 이상인 물고기들을 종류별로 분류하여 잡은 수, 최대 길이, 물고기의 종류를 출력하는 SQL문을 작성해주세요. 결과는 물고기 종류에 대해 오름차순으로 정렬해주시고, 10cm이하의 물고기들은 10cm로 취급하여 평균 길이를 구해주세요.
+
+-- 컬럼명은 물고기의 종류 'FISH_TYPE', 잡은 수 'FISH_COUNT', 최대 길이 'MAX_LENGTH'로 해주세요.
+
+with r as (
+    select fish_type, ifnull(length, 10) as length, time
+    from fish_info
+)
+
+select count(*) as fish_count, max(length) as max_length, fish_type 
+from r
+group by fish_type
+having max(length) >= 33
+order by fish_type
