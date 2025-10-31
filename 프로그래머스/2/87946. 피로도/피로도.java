cb325f4f603884c7ab2a184d66dfa7be177f95d0
Jaeyoung Mun <124485699+answodud@users.noreply.github.com>
2025-10-31 18:26:52 +0900

    [level 2] Title: 피로도, Time: 7.00 ms, Memory: 83.8 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,10 +1,12 @@
 class Solution {
+    int k;
     int[][] dungeons;
     boolean[] visited;
-    int max_cnt = -1;
+    int max_cnt = 0;
     
     public int solution(int k, int[][] dungeons) {
         int answer = -1;
+        this.k = k;
         this.dungeons = dungeons;
         visited = new boolean[dungeons.length];
         
@@ -13,20 +15,20 @@ class Solution {
         return max_cnt;
     }
     
-    public void dfs(int cur, int depth, int cnt){
+    public void dfs(int piro, int depth, int cnt){
+        
         if(depth >= dungeons.length){
-            if(cnt > max_cnt){
-                max_cnt = cnt;
-            }
+            max_cnt = Math.max(max_cnt, cnt);
             return;
         }
+        
         for(int i = 0; i < dungeons.length; i++){
             if(!visited[i]){
                 visited[i] = true;
-                if(dungeons[i][0] <= cur){
-                    dfs(cur - dungeons[i][1], depth + 1, cnt + 1);
+                if(dungeons[i][0] <= piro){
+                    dfs(piro - dungeons[i][1], depth + 1, cnt + 1);
                 } else {
-                    dfs(cur, depth + 1, cnt);
+                    dfs(piro, depth + 1, cnt);
                 }
                 visited[i] = false;
             }
