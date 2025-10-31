7b40f9451ef403745b0c6266631815e7ed008eb6
Jaeyoung Mun <124485699+answodud@users.noreply.github.com>
2025-10-31 19:23:24 +0900

    [level 2] Title: 모음 사전, Time: 25.45 ms, Memory: 72.2 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,36 +1,32 @@
 class Solution {
-    
-    String dic = "AEIOU";
-    int cnt = 0;
-    boolean found = false;
+    String str = "AEIOU";
     String word;
+    int cnt = 0;
     int answer = 0;
-    
+    boolean found = false;
     public int solution(String word) {
-
+        
         this.word = word;
+        
         dfs("");
         
         return answer;
     }
     
-    public void dfs(String cur){
-        if(cur.length() > 0){
-            cnt++;
-            if(cur.equals(word)){
-                found = true;
-                answer = cnt;
-                return;
-            }
-        }
-        
-        if(found == true || cur.length() == 5){
+    public void dfs(String s){
+        if(s.equals(word)){
+            found = true;
+            answer = cnt;
             return;
         }
         
-        for(int i = 0; i < dic.length(); i++){
-            dfs(cur + dic.charAt(i));
+        cnt++;
+        if(found || s.length() >= 5){
+            return;
         }
-        
+
+        for(int i = 0; i < str.length(); i++){
+            dfs(s + str.charAt(i));
+        }
     }
 }
