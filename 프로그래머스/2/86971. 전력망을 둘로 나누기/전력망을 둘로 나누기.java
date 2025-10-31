04cd2d79f1f480f51ae69a2d0ff385963d6504bf
Jaeyoung Mun <124485699+answodud@users.noreply.github.com>
2025-10-31 18:49:09 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 1.93 ms, Memory: 82.6 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -5,22 +5,22 @@ class Solution {
     ArrayList<Integer>[] graph;
     boolean[] visited;
     public int solution(int n, int[][] wires) {
-        int answer = -1;
-        
+        int min = n;
         graph = new ArrayList[n+1];
+        
+        
         for(int i = 0; i <= n; i++){
             graph[i] = new ArrayList<>();
         }
         
-        for(int[] wire : wires){
-            int v1 = wire[0];
-            int v2 = wire[1];
+        for(int i = 0; i < wires.length; i++){
+            int v1 = wires[i][0];
+            int v2 = wires[i][1];
             graph[v1].add(v2);
             graph[v2].add(v1);
         }
         
-        int min = 100001;
-        
+        // 처신을 잘하자!
         for(int i = 0; i < wires.length; i++){
             int v1 = wires[i][0];
             int v2 = wires[i][1];
@@ -28,27 +28,24 @@ class Solution {
             graph[v2].remove(Integer.valueOf(v1));
             
             visited = new boolean[n+1];
-            int n1 = dfs(1);
-            min = Math.min(Math.abs(n1 - (n - n1)), min);
             
+            int cnt = dfs(1);
+            min = Math.min(Math.abs(cnt - (n - cnt)), min);
             graph[v1].add(v2);
             graph[v2].add(v1);
         }
-        
         
         return min;
     }
     
     public int dfs(int v){
-        int cnt = 1;
         visited[v] = true;
+        int cnt = 1;
         for(int next : graph[v]){
-            if(!visited[next]){
-                visited[next] = true;
-                cnt += dfs(next);
+                if(!visited[next]){
+                    cnt += dfs(next);
+                }
             }
-            
-        }
         
         return cnt;
     }
