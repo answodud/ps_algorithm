c48386d7d914062521043d903a8f22179e01ec97
Jaeyoung Mun <124485699+answodud@users.noreply.github.com>
2025-10-31 18:07:45 +0900

    [level 2] Title: 소수 찾기, Time: 21.51 ms, Memory: 85 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,53 +1,51 @@
 import java.util.*;
 
 class Solution {
-    String numbers;
+    
+    HashSet<Integer> set = new HashSet<>();
     boolean[] visited;
-    HashSet<Integer> hashSet = new HashSet<>();
+    String numbers;
     
     public int solution(String numbers) {
         int answer = 0;
-        this.numbers = numbers;
         visited = new boolean[numbers.length()];
+        this.numbers = numbers;
         
+        dfs("", 0);
         
-        int[] num = new int[numbers.length()];
-        
-        for(int i = 0; i < numbers.length(); i++){
-            num[i] = Integer.valueOf(numbers.charAt(i));
-        }
-        
-        dfs("");
-        
-        
-        return hashSet.size();
+        return set.size();
     }
     
-    public void dfs(String cur){
-        if(cur.length() > 0){
-            int x = Integer.parseInt(cur);
-        if(isPrime(x)){
-            hashSet.add(x);
+    public void dfs(String x, int depth){
+        
+        if(!x.equals("") && isPrime(Integer.valueOf(x))){
+            set.add(Integer.valueOf(x));
         }
+        
+        if(depth >= numbers.length()){
+            return;
         }
+        
         for(int i = 0; i < numbers.length(); i++){
             if(!visited[i]){
                 visited[i] = true;
-                dfs(cur + numbers.charAt(i));
+                dfs(x + numbers.charAt(i), depth + 1);
                 visited[i] = false;
             }
         }
+        
     }
     
-    public boolean isPrime(int num){
-        if(num < 2){
+    public boolean isPrime(int x){
+        if(x == 0 || x == 1){
             return false;
         }
-        for(int i = 2; i <= Math.sqrt(num); i++){
-            if(num % i == 0){
+        for(int i = 2; i <= Math.sqrt(x); i++){
+            if(x % i == 0){
                 return false;
             }
         }
         return true;
     }
+    
 }
