37a8a0a1b5654609df742ae877ce67e0ff1f71fd
Jaeyoung Mun <124485699+answodud@users.noreply.github.com>
2025-10-31 18:15:31 +0900

    [level 2] Title: 카펫, Time: 0.03 ms, Memory: 83 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -2,26 +2,24 @@ class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
         
-        int yellow_x = 0;
-        int yellow_y = 0;
-        
+        int yellow_y = 1;
+        int yellow_x;
         while(true){
-            yellow_x++;
-            if(yellow % yellow_x == 0){
-                yellow_y = yellow / yellow_x;
-            }
-            if(((yellow_x + 2) * (yellow_y + 2) - yellow) == brown){
-                System.out.println(yellow_x + " " + yellow_y);
-                if(yellow_x >= yellow_y){
-                    answer[0] = yellow_x + 2;
-                    answer[1] = yellow_y + 2;
-                } else {
-                    answer[0] = yellow_y + 2;
-                    answer[1] = yellow_x + 2;
+            if(yellow % yellow_y == 0){
+                yellow_x = yellow / yellow_y;
+                if((yellow_x + 2) * (yellow_y + 2) == (brown + yellow)){
+                    if(yellow_x > yellow_y){
+                        answer[0] = yellow_x + 2;
+                        answer[1] = yellow_y + 2;
+                        
+                    } else {
+                        answer[1] = yellow_x + 2;
+                        answer[0] = yellow_y + 2;
+                    }
+                    break;
                 }
-                
-                break;
             }
+            yellow_y++;
         }
         
         return answer;
